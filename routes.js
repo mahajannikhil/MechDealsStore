af27c15789abee5109e49dd5e76fa8be82475111
nikhil.mahajan <nikhil.mahajan@pepcus.com>
2021-01-04 11:48:59 +0530

    Initial Commit

---
+++ routes.js
@@ -0,0 +1,18 @@
+const { Router } = require('@xdn/core/router')
+const { nextRoutes } = require('@xdn/next')
+const { API, SSR, cacheResponse } = require('./cache')
+const prerenderRequests = require('./xdn/prerenderRequests')
+
+module.exports = new Router()
+  .prerender(prerenderRequests)
+  .match('/service-worker.js', ({ serviceWorker }) => {
+    serviceWorker('.next/static/service-worker.js')
+  })
+  .match('/', cacheResponse(SSR))
+  .match('/api', cacheResponse(API))
+  .match('/s/:categorySlug*', cacheResponse(SSR))
+  .match('/api/s/:categorySlug*', cacheResponse(API))
+  .match('/p/:productId', cacheResponse(SSR))
+  .match('/api/p/:productId', cacheResponse(API))
+  .use(nextRoutes)
+  .fallback(({ proxy }) => proxy('legacy'))
